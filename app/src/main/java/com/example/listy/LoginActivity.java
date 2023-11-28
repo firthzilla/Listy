d8c6edbcce04228bb37b0be39a39c2a92e120283
firthzilla <alexfirth.af@gmail.com>
2023-11-28 15:55:44 -0500

    Fixed the LoginActivity to actually authenticate users using email and password via firebase

--- app/src/main/java/com/example/listy/LoginActivity.java
+++ app/src/main/java/com/example/listy/LoginActivity.java
@@ -3,7 +3,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,7 @@ import java.time.chrono.MinguoChronology;
 public class LoginActivity extends AppCompatActivity {
 
   private FirebaseAuth firebaseAuth;
+  private EditText editTextEmail, editTextPassword;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,6 +27,10 @@ public class LoginActivity extends AppCompatActivity {
     // Initialize Firebase
     firebaseAuth = FirebaseAuth.getInstance();
 
+    // Initialize views
+    editTextEmail = findViewById(R.id.editTextEmail);
+    editTextPassword = findViewById(R.id.editTextPassword);
+
     // Check if the user is already authenticated
     FirebaseUser currentUser = firebaseAuth.getCurrentUser();
     if (currentUser != null) {
@@ -31,17 +38,16 @@ public class LoginActivity extends AppCompatActivity {
       startActivity(new Intent(LoginActivity.this, MainActivity.class));
       finish();
     }
-
     Button loginButton = findViewById(R.id.login_button);
     loginButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        // Add your login logic here using Firebase Authentication
-        // After successful login, navigate to the home activity
-        startActivity(new Intent(LoginActivity.this, MainActivity.class));
-        finish();
+        // Call the login method when the login button is clicked
+        loginUser();
       }
     });
+
+
     TextView signUpTextView = findViewById(R.id.textViewSignUp);
     signUpTextView.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -51,5 +57,28 @@ public class LoginActivity extends AppCompatActivity {
         // Navigate the user to RegistrationActivity
       }
     });
+  }
+
+  private void loginUser() {
+    // Add your login logic here using Firebase Authentication
+    // After successful login, navigate to the home activity
+    String email = editTextEmail.getText().toString().trim();
+    String password = editTextPassword.getText().toString().trim();
+
+    if (email.isEmpty() || password.isEmpty()) {
+      Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+      return;
+    }
+    firebaseAuth.signInWithEmailAndPassword(email, password)
+      .addOnCompleteListener(this, task -> {
+        if (task.isSuccessful()) {
+          // Login successful, navigate to the home activity
+          startActivity(new Intent(LoginActivity.this, MainActivity.class));
+          finish();
+        } else {
+          // Login failed, show an error message
+          Toast.makeText(this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+        }
+      });
   }
 }
